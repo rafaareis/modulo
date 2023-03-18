08af8c735b7b146e880e24efc7ffdd2cef0d75e3
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-03-18 19:05:20 -0300

    Commit 18032023-1902: Finalizando o exercício

--- Módulo 7 - Exercícicio/main.js
+++ Módulo 7 - Exercícicio/main.js
@@ -1,3 +1,5 @@
+const container = document.getElementById('container-id');
+console.log(container);
 //formulário
 const formBlock = document.getElementById('form-block');
 console.log(formBlock);
@@ -20,17 +22,50 @@ console.log(comparisonValidation(2, 1));
 
 console.log(comparisonValidation(2, 5));
 
+const mensagemDeSucesso = document.querySelector('.success-message');
+console.log(mensagemDeSucesso);
+
+const mensagemDeErro = document.querySelector('.error-message');
+console.log(mensagemDeErro);
+
+container.addEventListener('click', function(e) {
+    e.target.value;
+    console.log(e.target.value);
+
+    mensagemDeErro.style.display = 'none';
+    mensagemDeSucesso.style.display = 'none';
+})
+
 formBlock.addEventListener('submit', function(e) {
     //remove o comportamento do formulário de recarregar a página
     e.preventDefault();
 
     comparision = comparisonValidation(numeroA, numeroB);
     console.log(comparision);
+
+
+    if(comparision) {
+     mensagemDeSucesso.style.display = 'block';
+     mensagemDeErro.style.display = 'none';
+     numeroA.value = '';
+     numeroB.value = '';    
+    } else {
+        mensagemDeErro.style.display = 'block';
+        mensagemDeSucesso.style.display = 'none';
+        numeroA.value = '';
+        numeroB.value = '';    
+   
+    }
+    
+
 })
 
 numeroA.addEventListener('click', function(e) {
     e.target.value;
     console.log(e.target.value);
+
+    mensagemDeErro.style.display = 'none';
+    mensagemDeSucesso.style.display = 'none';
 })
 
 numeroB.addEventListener('click', function(e) {
