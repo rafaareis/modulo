ab22d56786eb7b02e5a2d914a2d9eb299d5316de
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-03-18 12:01:50 -0300

    Commit 18032023-1201: Adicionando atualizações

--- Módulo 7 - Exercícicio/main.js
+++ Módulo 7 - Exercícicio/main.js
@@ -1,12 +1,46 @@
 //formulário
 const formBlock = document.getElementById('form-block');
 console.log(formBlock);
-
+//Seleciona input: primeiro número;
 const numeroA = document.getElementById('numero-a');
-console.log(numeroA)
+////Seleciona input: segundo número;
+const numeroB = document.getElementById('numero-b');
+//Comparação
+let comparision = false;
+
+//Formula de validação de comparação
+function comparisonValidation(numeroA, numeroB) {
+    const a = numeroA.value;
+    const b = numeroB.value;
+    
+    return b > a;
+}
+
+console.log(comparisonValidation(2, 1));
+
+console.log(comparisonValidation(2, 5));
 
 formBlock.addEventListener('submit', function(e) {
     //remove o comportamento do formulário de recarregar a página
     e.preventDefault();
+
+    comparision = comparisonValidation(numeroA, numeroB);
+    console.log(comparision);
 })
+
+numeroA.addEventListener('click', function(e) {
+    e.target.value;
+    console.log(e.target.value);
+})
+
+numeroB.addEventListener('click', function(e) {
+    e.target.value;
+    console.log(e.target.value);
+})
+
+
+
+
+
+
 
